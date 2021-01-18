5800dd5f2e395ffb8f482320c3f388a933cb9a60
DushkaF <fdsaq1234@ya.ru>
2021-01-18 23:25:21 +0300

    Add JSON senging + network list visualise

--- software_ESP/NixieESP32/NixieESP32.ino
+++ software_ESP/NixieESP32/NixieESP32.ino
@@ -3,18 +3,68 @@
 #include <AsyncTCP.h>
 #include "ESPAsyncWebServer.h"
 #include <SPIFFS.h>
+#include <ArduinoJson.h>
+
+
+
+unsigned long last_time = 0;
 
 DNSServer dnsServer;
 AsyncWebServer server(80);
+
+String getNetworksJSON() {
+  StaticJsonDocument<256> APlist;
+  JsonArray names = APlist.createNestedArray("name");
+  
+  Serial.println("scan start");
+  // WiFi.scanNetworks will return the number of networks found
+  int n = WiFi.scanNetworks();
+  Serial.println("scan done");
+  if (n == 0) {
+    Serial.println("no networks found");
+  } else {
+    Serial.print(n);
+    Serial.println(" networks found");
+    for (int i = 0; i < n; ++i) {
+      names.add(WiFi.SSID(i));
+      // Print SSID and RSSI for each network found
+      /*Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.print(WiFi.SSID(i));
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(i));
+      Serial.print(")");
+      Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN) ? " " : "*");
+      delay(10);*/
+    }
+  }
+  Serial.println("");
+
+  String JsonLine = "";
+  serializeJson(APlist, JsonLine);
+  return JsonLine;
+}
+
 
 class CaptiveRequestHandler : public AsyncWebHandler {
   public:
     CaptiveRequestHandler() {
       /* THIS IS WHERE YOU CAN PLACE THE CALLS */
 
-      server.on("/style/style.css", HTTP_GET, [](AsyncWebServerRequest *request) {
+      server.on("/style/style.css", HTTP_GET, [](AsyncWebServerRequest * request) {
         AsyncWebServerResponse* response = request->beginResponse(SPIFFS, "/inner_page/style/style.css", "text/css");
-         request->send(response);
+        request->send(response);
+      });
+
+      server.on("/js/script.js", HTTP_GET, [](AsyncWebServerRequest * request) {
+        AsyncWebServerResponse* response = request->beginResponse(SPIFFS, "/inner_page/js/script.js");
+        request->send(response);
+      });
+
+      server.on("/APlist.json", HTTP_GET, [](AsyncWebServerRequest * request) {
+        //AsyncWebServerResponse* response = request->beginResponse(SPIFFS, "/inner_page/APlist.json", "application/json");
+        AsyncWebServerResponse* response = request->beginResponse(200, "application/json", getNetworksJSON());
+        request->send(response);
       });
     }
     virtual ~CaptiveRequestHandler() {}
@@ -30,7 +80,7 @@ class CaptiveRequestHandler : public AsyncWebHandler {
 };
 
 void setup() {
-
+  WiFi.mode(WIFI_AP_STA);
   Serial.begin(115200);
 
   if (!SPIFFS.begin()) {
@@ -45,11 +95,16 @@ void setup() {
 
   /*server.on("/image1", HTTP_GET, [](AsyncWebServerRequest * request) {
     request->send(SPIFFS, "/image1.jpg", "image/jpg"); // this part has been modified
-  });*/
+    });*/
 
   server.begin();
 }
 
 void loop() {
   dnsServer.processNextRequest();
+
+  /*if (millis() - last_time >= 5000) {
+    Serial.println(getNetworksJSON());
+    last_time = millis();
+  }*/
 }
